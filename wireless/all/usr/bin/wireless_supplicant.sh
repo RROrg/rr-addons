77dca1f68e49aa18db45d8379b15c6c11e583676
Ing <wjz304@qq.com>
2024-01-12 12:16:55 +0800

    fix iwlwifi

--- wireless/all/usr/bin/wireless_supplicant.sh
+++ wireless/all/usr/bin/wireless_supplicant.sh
@@ -6,11 +6,32 @@
 # See /LICENSE for more information.
 #
 
+# TODO:
+# 2024-01-11T06:00:34+08:00 test synoscgi_SYNO.Core.Network.TrafficControl.Rules_1_load[28982]: utils.cpp:44 can not get iftype for wlan0
+# [ 1093.186544] SYNO.Core.Syste[29358]: segfault at 0 ip 00007f4bef3bef59 sp 00007ffd65d9fe48 error 4 in libc.so.6[7f4bef298000+14c000]
+
 NAME="${1}"
 SSID="${2}"
 PSK="${3}"
 
 if [ -n "${NAME}" -a -n "${SSID}" -a -n "${PSK}" ]; then
+  # fix iwlwifi module
+  if [ -z "$(ls /sys/class/net/wlan*)" ] && lsmod | grep -q ^iwlwifi; then
+    rmmod iwlmvm 2>/dev/null
+    rmmod iwlwifi 2>/dev/null
+    rmmod mac80211 2>/dev/null
+    rmmod cfg80211 2>/dev/null
+    rmmod libarc4 2>/dev/null
+
+    insmod /lib/modules/libarc4.ko
+    insmod /lib/modules/cfg80211.ko
+    insmod /lib/modules/mac80211.ko
+    insmod /lib/modules/iwlwifi.ko
+    insmod /lib/modules/iwlmvm.ko
+
+    sleep 1
+  fi
+
   if [ ! -d "/sys/class/net/${NAME}" ]; then
     echo "Interface ${NAME} not found"
     exit 1
