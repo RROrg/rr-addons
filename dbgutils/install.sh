1aeca145ffab2b06627b9d6b7cf382b07baa5997
Ing <wjz304@qq.com>
2024-01-19 01:27:50 +0800

    update dbgutils

--- dbgutils/install.sh
+++ dbgutils/install.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env ash
 
-if [ "${1}" = "early" ]; then
+case "${1}" in
+"early" | "jrExit")
+  echo "Installing addon dbgutils ${1}"
   [ ! -L /usr/sbin/modinfo ] && ln -vsf /usr/bin/kmod /usr/sbin/modinfo
-  loader-logs.sh early
-  echo "Starting ttyd..."
+  loader-logs.sh "${1}"
+  ;;
+"rcExit")
+  echo "Installing addon dbgutils ${1}"
+  [ ! -L /usr/sbin/modinfo ] && ln -vsf /usr/bin/kmod /usr/sbin/modinfo
+  loader-logs.sh "${1}"
+  echo "Starting ttyd ..."
+  if /usr/bin/lsof -Pi :7681 -sTCP:LISTEN -t >/dev/null; then
+    echo "Port 7681 is already in use. Terminating the existing process..."
+    /usr/bin/lsof -i :7681
+  fi
   /usr/sbin/ttyd /usr/bin/ash -l &
+  echo "Starting dufs ..."
+  if /usr/bin/lsof -Pi :7304 -sTCP:LISTEN -t >/dev/null; then
+    echo "Port 7304 is already in use. Terminating the existing process..."
+    /usr/bin/lsof -i :7304
+  fi
   /usr/sbin/dufs -A -p 7304 / &
-elif [ "${1}" = "jrExit" ]; then
+  ;;
+"late")
+  echo "Installing addon dbgutils ${1}"
   [ ! -L /usr/sbin/modinfo ] && ln -vsf /usr/bin/kmod /usr/sbin/modinfo
-  loader-logs.sh jrExit
-elif [ "${1}" = "rcExit" ]; then
-  [ ! -L /usr/sbin/modinfo ] && ln -vsf /usr/bin/kmod /usr/sbin/modinfo
-  loader-logs.sh rcExit
-elif [ "${1}" = "late" ]; then
+  loader-logs.sh "${1}"
+
   echo "Installing addon dbgutils"
-  echo "Killing ttyd..."
+  echo "Killing ttyd ..."
   /usr/bin/killall ttyd
+  echo "Killing dufs ..."
   /usr/bin/killall dufs
 
   echo "Copying utils"
@@ -28,21 +44,27 @@ elif [ "${1}" = "late" ]; then
   cp -vf /usr/bin/lsof /tmpRoot/usr/bin/
   cp -vf /usr/bin/loader-logs.sh /tmpRoot/usr/bin/
   cp -vf /usr/sbin/ttyd /tmpRoot/usr/sbin/
+  cp -vf /usr/sbin/dufs /tmpRoot/usr/sbin/
 
   loader-logs.sh late
 
   DEST="/tmpRoot/lib/systemd/system/savelogs.service"
-  echo "[Unit]"                                                       >${DEST}
-  echo "Description=RR save logs for debug"                          >>${DEST}
-  echo                                                               >>${DEST}
-  echo "[Service]"                                                   >>${DEST}
-  echo "Type=oneshot"                                                >>${DEST}
-  echo "RemainAfterExit=true"                                        >>${DEST}
-  echo "ExecStop=/bin/loader-logs.sh dsm"                            >>${DEST}
-  echo                                                               >>${DEST}
-  echo "[Install]"                                                   >>${DEST}
-  echo "WantedBy=multi-user.target"                                  >>${DEST}
+  echo "[Unit]"                                      >${DEST}
+  echo "Description=RR save logs for debug"         >>${DEST}
+  echo                                              >>${DEST}
+  echo "[Service]"                                  >>${DEST}
+  echo "Type=oneshot"                               >>${DEST}
+  echo "RemainAfterExit=true"                       >>${DEST}
+  echo "ExecStop=/bin/loader-logs.sh dsm"           >>${DEST}
+  echo                                              >>${DEST}
+  echo "[Install]"                                  >>${DEST}
+  echo "WantedBy=multi-user.target"                 >>${DEST}
 
   mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
   ln -vsf /lib/systemd/system/savelogs.service /tmpRoot/lib/systemd/system/multi-user.target.wants/savelogs.service
-fi
+  ;;
+*)
+  echo "dbgutils nothing to do with argument ${1}"
+  exit 0
+  ;;
+esac
