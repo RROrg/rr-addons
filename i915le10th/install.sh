ff8de0adc25def2ebed2c92db05262294969309d
Ing <wjz304@qq.com>
2024-02-25 18:45:07 +0800

    fix i915

--- i915le10th/install.sh
+++ i915le10th/install.sh
@@ -15,13 +15,16 @@
 
 if [ "${1}" = "patches" ]; then
   echo "Installing addon i915le10th - ${1}"
+
   if [ -n "${2}" ]; then
     GPU="$(echo "${2}" | sed 's/://g' | tr '[:upper:]' '[:lower:]')"
   else
     GPU="$(lspci -n | grep 0300 | grep 8086 | cut -d " " -f 3 | sed 's/://g')"
   fi
   [ -z "${GPU}" -o $(echo -n "${GPU}" | wc -c) -ne 8 ] && echo "GPU is not detected" && exit 0
-  [ ! -f "/usr/lib/modules/i915.ko" ] && echo "i915.ko does not exist" && exit 0
+
+  KO_FILE="/usr/lib/modules/i915.ko"
+  [ ! -f "${KO_FILE}" ] && echo "i915.ko does not exist" && exit 0
 
   if [ -n "${2}" ] || grep -iq ${GPU} /usr/bin/i915ids 2>/dev/null; then
     isLoad=0
@@ -32,8 +35,8 @@ if [ "${1}" = "patches" ]; then
     GPU_DEF="86800000923e0000"
     GPU_BIN="${GPU:2:2}${GPU:0:2}0000${GPU:6:2}${GPU:4:2}0000"
     echo "GPU:${GPU} GPU_BIN:${GPU_BIN}"
-    cp -vf "/usr/lib/modules/i915.ko" "/usr/lib/modules/i915.ko.bak"
-    sed -i "s/${GPU_DEF}/${GPU_BIN}/; s/308201f706092a86.*70656e6465647e0a//" "/usr/lib/modules/i915.ko"
+    cp -vf "${KO_FILE}" "${KO_FILE}.bak"
+    xxd -c $(xxd -p "${KO_FILE}.bak" | wc -c) -p "${KO_FILE}.bak" | sed "s/${GPU_DEF}/${GPU_BIN}/; s/308201f706092a86.*70656e6465647e0a//" | xxd -r -p > "${KO_FILE}"
     [ "${isLoad}" = "1" ] && /usr/sbin/modprobe "/usr/lib/modules/i915.ko"
   fi
 elif [ "${1}" = "late" ]; then
