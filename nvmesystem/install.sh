9d0f5515406c7929c9cd13bb83aa1d1afbe7f95d
Ing <wjz304@qq.com>
2024-03-12 21:50:01 +0800

    add nvmesystem

---
+++ nvmesystem/install.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env ash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# From：jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
+#
+
+if [ "${1}" = "early" ]; then
+  echo "Installing addon nvmesystem - ${1}"
+
+  # System volume is assembled with SSD Cache only, please remove SSD Cache and then reboot
+  sed -i "s/support_ssd_cache=.*/support_ssd_cache=\"no\"/" /etc/synoinfo.conf /etc.defaults/synoinfo.conf
+
+  # [CREATE][failed] Raidtool initsys
+  SO_FILE="/usr/syno/bin/scemd"
+  [ ! -f "${SO_FILE}.bak" ] && cp -vf "${SO_FILE}" "${SO_FILE}.bak"
+  cp -f "${SO_FILE}" "${SO_FILE}.tmp"
+  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
+    sed "s/dcfcffff4584ed74b7488b4c24083b01/dcfcffff4584ed75b7488b4c24083b01/" |
+    xxd -r -p >"${SO_FILE}" 2>/dev/null
+  rm -f "${SO_FILE}.tmp"
+
+elif [ "${1}" = "late" ]; then
+  echo "Installing addon nvmesystem - ${1}"
+  mkdir -p "/tmpRoot/usr/rr/addons/"
+  cp -vf "${0}" "/tmpRoot/usr/rr/addons/"
+
+  # disk/shared_disk_info_enum.c::84 Failed to allocate list in SharedDiskInfoEnum, errno=0x900.
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ ! -f "${SO_FILE}.bak" ] && cp -vf "${SO_FILE}" "${SO_FILE}.bak"
+
+  cp -vf "${SO_FILE}" "${SO_FILE}.tmp"
+  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
+    sed "s/0f95c00fb6c0488b9424081000006448/0f94c00fb6c0488b9424081000006448/; s/ffff89c18944240c8b44240809e84409/ffff89c18944240c8b44240890904409/" |
+    xxd -r -p >"${SO_FILE}" 2>/dev/null
+  rm -f "${SO_FILE}.tmp"
+
+  # Create storage pool page without RAID type.
+  cp -vf /usr/bin/nvmesystem.sh /tmpRoot/usr/bin/nvmesystem.sh
+
+  DEST="/tmpRoot/usr/lib/systemd/system/nvmesystem.service"
+  echo "[Unit]"                                          >${DEST}
+  echo "Description=Modify storage panel"               >>${DEST}
+  echo "After=multi-user.target"                        >>${DEST}
+  echo                                                  >>${DEST}
+  echo "[Service]"                                      >>${DEST}
+  echo "Type=oneshot"                                   >>${DEST}
+  echo "RemainAfterExit=true"                           >>${DEST}
+  echo "ExecStart=/usr/bin/nvmesystem.sh"               >>${DEST}
+  echo                                                  >>${DEST}
+  echo "[Install]"                                      >>${DEST}
+  echo "WantedBy=multi-user.target"                     >>${DEST}
+
+  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/nvmesystem.service /tmpRoot/lib/systemd/system/multi-user.target.wants/nvmesystem.service
+
+elif [ "${1}" = "uninstall" ]; then
+  echo "Installing addon nvmesystem - ${1}"
+
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ -f "${SO_FILE}.bak" ] && mv -f "${SO_FILE}.bak" "${SO_FILE}"
+
+  rm -f "/tmpRoot/lib/systemd/system/multi-user.target.wants/nvmesystem.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/nvmesystem.service"
+
+  [ ! -f "/tmpRoot/usr/rr/revert.sh" ] && echo '#!/usr/bin/env bash' >/tmpRoot/usr/rr/revert.sh && chmod +x /tmpRoot/usr/rr/revert.sh
+  echo "/usr/bin/nvmesystem.sh -r" >>/tmpRoot/usr/rr/revert.sh
+  echo "rm -f /usr/bin/nvmesystem.sh" >>/tmpRoot/usr/rr/revert.sh
+fi
