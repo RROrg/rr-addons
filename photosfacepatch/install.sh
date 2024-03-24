8d597a0f12113748afd5cf7e90bd04dbf14b57e4
Ing <wjz304@qq.com>
2024-03-25 05:28:18 +0800

    update photosfacepatch

--- photosfacepatch/install.sh
+++ photosfacepatch/install.sh
@@ -10,36 +10,45 @@ if [ "${1}" = "late" ]; then
   echo "Installing addon photosfacepatch - ${1}"
   mkdir -p "/tmpRoot/usr/rr/addons/"
   cp -vf "${0}" "/tmpRoot/usr/rr/addons/"
-  
-  if [ -f "/tmpRoot/usr/lib/libsynosdk.so.7" ]; then
-    if [ ! -f "/tmpRoot/usr/lib/libsynosdk.so.7.bak" ]; then
-      echo "Backup libsynosdk.so.7"
-      cp -vfp "/tmpRoot/usr/lib/libsynosdk.so.7" "/tmpRoot/usr/lib/libsynosdk.so.7.bak"
+
+  SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
+  if [ -f "${SO_FILE}" ]; then
+    if [ ! -f "${SO_FILE}.bak" ]; then
+      echo "Backup ${SO_FILE}"
+      cp -vfp "${SO_FILE}" "${SO_FILE}.bak"
     fi
     echo "Patching libsynosdk.so.7"
-    PatchELFSharp "/tmpRoot/usr/lib/libsynosdk.so.7" "SYNOFSIsRemoteFS" "B8 00 00 00 00 C3"
+    PatchELFSharp "${SO_FILE}" "SYNOFSIsRemoteFS" "B8 00 00 00 00 C3"
   else
     echo "libsynosdk.so.7 not found"
   fi
-  if [ -f "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so" ]; then
-    if [ ! -f "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so.bak" ]; then
-      echo "Backup libsynophoto-plugin-platform.so"
-      cp -vfp "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so" "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so.bak"
+  SO_FILE="/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
+  if [ -f "${SO_FILE}" ]; then
+    if [ ! -f "${SO_FILE}.bak" ]; then
+      echo "Backup ${SO_FILE}"
+      cp -vfp "${SO_FILE}" "${SO_FILE}.bak"
     fi
-    echo "Patching libsynophoto-plugin-platform.so"
-    PatchELFSharp "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so" "_ZN9synophoto6plugin8platform20IsSupportedIENetworkEv" "B8 00 00 00 00 C3"
+    echo "Patching ${${SO_FILE}}"
+    # support face and concept
+    PatchELFSharp "${SO_FILE}" "_ZN9synophoto6plugin8platform20IsSupportedIENetworkEv" "B8 00 00 00 00 C3"
+    # force to support concept
+    PatchELFSharp "${SO_FILE}" "_ZN9synophoto6plugin8platform18IsSupportedConceptEv" "B8 01 00 00 00 C3"
+    # force no Gpu
+    PatchELFSharp "${SO_FILE}" "_ZN9synophoto6plugin8platform23IsSupportedIENetworkGpuEv" "B8 00 00 00 00 C3"
   else
-    echo "libsynophoto-plugin-platform.so not found"
+    echo "${SO_FILE} not found"
   fi
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon photosfacepatch - ${1}"
 
-  if [ -f "/tmpRoot/usr/lib/libsynosdk.so.7.bak" ]; then
-    echo "Restore libsynosdk.so.7"
-    mv -f "/tmpRoot/usr/lib/libsynosdk.so.7.bak" "/tmpRoot/usr/lib/libsynosdk.so.7"
+  SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
+  if [ -f "${SO_FILE}.bak" ]; then
+    echo "Restore ${SO_FILE}"
+    mv -f "${SO_FILE}.bak" "${SO_FILE}"
   fi
-  if [ -f "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so.bak" ]; then
-    echo "Restore libsynophoto-plugin-platform.so"
-    mv -f "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so.bak" "/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
+  SO_FILE="/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
+  if [ -f "${SO_FILE}.bak" ]; then
+    echo "Restore ${SO_FILE}"
+    mv -f "${SO_FILE}.bak" "${SO_FILE}"
   fi
 fi
