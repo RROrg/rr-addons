fccc00b41875a26dacb96080f431988e4b309343
Ing <wjz304@qq.com>
2024-03-28 03:31:59 +0800

    add notify

---
+++ notify/all/usr/bin/notify.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+if [ "${1}" = "-r" ]; then
+  TEXTS_PATH="/usr/local/share/notification/rr"
+  CACHE_PATH="/var/cache/texts/rr"
+  [ -d "${TEXTS_PATH}" ] && rm -rf "${TEXTS_PATH}"
+  [ -d "${CACHE_PATH}" ] && rm -rf "${CACHE_PATH}"
+  /usr/syno/bin/notification_utils --remove_category_db_file rr
+  /usr/syno/bin/notification_utils --sync_setting_db
+else
+  TEXTS_PATH="/usr/local/share/notification/rr"
+  CACHE_PATH="/var/cache/texts/rr"
+  for F in $(ls "/usr/syno/synoman/webman/texts" 2>/dev/null); do
+    rm -rf "${TEXTS_PATH}/${F}"
+    mkdir -p "${TEXTS_PATH}/${F}"
+    echo -en '[rr_notify]\nCategory: System\nLevel: NOTIFICATION_INFO\nDesktop: %NOTIFICATION%\n\n\n' >>"${TEXTS_PATH}/${F}/mails"
+    echo -en '[rr_notify_subject]\nCategory: System\nLevel: NOTIFICATION_INFO\nDesktop: %NOTIFICATION%\nSubject: %NOTIFICATION%\n\n%SUBJECT%\n\nFrom %HOSTNAME%\n\n\n' >>"${TEXTS_PATH}/${F}/mails"
+  done
+  /bin/rm -rf "${CACHE_PATH}"
+  /bin/mkdir -p /var/cache/texts
+  /bin/rsync -ar "${TEXTS_PATH}/" "${CACHE_PATH}"
+  /usr/syno/bin/notification_utils --remove_category_db_file rr
+  /usr/syno/bin/notification_utils --gen_category_db_file rr enu
+  /usr/syno/bin/notification_utils --sync_setting_db
+
+  # NOTIFICATION="RR Notify"
+  # synodsmnotify -e false -b false "@administrators" "rr_notify" "{\"%NOTIFICATION%\": \"${NOTIFICATION}\"}"
+  NOTIFICATION="RR Notify"
+  SUBJECT="Welcome to <a href=\\\"https://github.com/RROrg\\\" target=blank>RROrg</a>!"
+  synodsmnotify -e false -b false "@administrators" "rr_notify_subject" "{\"%NOTIFICATION%\": \"${NOTIFICATION}\", \"%SUBJECT%\": \"${SUBJECT}\"}"
+fi
---
+++ notify/install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env ash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+if [ "${1}" = "late" ]; then
+  echo "Installing addon notify - ${1}"
+  mkdir -p "/tmpRoot/usr/rr/addons/"
+  cp -vf "${0}" "/tmpRoot/usr/rr/addons/"
+  
+  cp -vf /usr/bin/notify.sh /tmpRoot/usr/bin/notify.sh
+
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/notify.service"
+  echo "[Unit]"                                          >${DEST}
+  echo "Description=rr notify"                          >>${DEST}
+  echo "After=multi-user.target"                        >>${DEST}
+  echo                                                  >>${DEST}
+  echo "[Service]"                                      >>${DEST}
+  echo "Type=oneshot"                                   >>${DEST}
+  echo "RemainAfterExit=yes"                            >>${DEST}
+  echo "ExecStart=/usr/bin/notify.sh"                   >>${DEST}
+  echo                                                  >>${DEST}
+  echo "[Install]"                                      >>${DEST}
+  echo "WantedBy=multi-user.target"                     >>${DEST}
+
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/notify.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/notify.service
+elif [ "${1}" = "uninstall" ]; then
+  echo "Installing addon notify - ${1}"
+
+  rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/notify.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/notify.service"
+
+  [ ! -f "/tmpRoot/usr/rr/revert.sh" ] && echo '#!/usr/bin/env bash' >/tmpRoot/usr/rr/revert.sh && chmod +x /tmpRoot/usr/rr/revert.sh
+  echo "/usr/bin/notify.sh -r" >>/tmpRoot/usr/rr/revert.sh
+  echo "rm -f /usr/bin/notify.sh" >>/tmpRoot/usr/rr/revert.sh
+fi
