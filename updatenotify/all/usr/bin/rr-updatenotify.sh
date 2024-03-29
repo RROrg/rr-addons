d5eaab2c3bb741e2288952c33618190e7dcead44
Ing <wjz304@qq.com>
2024-03-29 09:56:28 +0800

    add updatenotify

---
+++ updatenotify/all/usr/bin/rr-updatenotify.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+LOCALTAG="$(cat /usr/rr/VERSION 2>/dev/null | grep LOADERVERSION | cut -d'=' -f2)"
+if [ -z "${LOCALTAG}" ]; then
+  echo "Unknown bootloader version!"
+  exit 0
+fi
+
+URL="https://github.com/RROrg/rr"
+TAG=""
+if echo "$@" | grep -qw "\-p"; then
+  TAG="$(curl -skL --connect-timeout 10 "${URL}/tags" | grep /refs/tags/.*\.zip | head -1 | sed -r 's/.*\/refs\/tags\/(.*)\.zip.*$/\1/')"
+else
+  LATESTURL="$(curl -skL --connect-timeout 10 -w %{url_effective} -o /dev/null "${URL}/releases/latest")"
+  TAG="${LATESTURL##*/}"
+fi
+[ "${TAG:0:1}" = "v" ] && TAG="${TAG:1}"
+if [ -z "${TAG}" -o "${TAG}" = "latest" ]; then
+  echo "Error checking new version. TAG is ${TAG}"
+  exit 0
+fi
+if [ "${TAG}" = "${LOCALTAG}" ]; then
+  echo "Actual version is ${TAG}"
+  exit 0
+fi
+
+# NOTIFICATION="RR Notify"
+# synodsmnotify -e false -b false "@administrators" "rr_notify" "{\"%NOTIFICATION%\": \"${NOTIFICATION}\"}"
+NOTIFICATION="RR Update"
+SUBJECT="RR <a href=\\\"https://github.com/RROrg/rr/releases/tag/${TAG}\\\" target=blank>${TAG}</a> version has been released!"
+synodsmnotify -e false -b false "@administrators" "rr_notify_subject" "{\"%NOTIFICATION%\": \"${NOTIFICATION}\", \"%SUBJECT%\": \"${SUBJECT}\"}"
+
+exit 0
