40b5728bac6835b2aca284f22bfb30b57a1862e3
Ing <wjz304@qq.com>
2024-04-20 04:57:35 +0800

    mod photosfacepatch

--- photosfacepatch/install.sh
+++ photosfacepatch/install.sh
@@ -11,17 +11,6 @@ if [ "${1}" = "late" ]; then
   mkdir -p "/tmpRoot/usr/rr/addons/"
   cp -vf "${0}" "/tmpRoot/usr/rr/addons/"
 
-  SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
-  if [ -f "${SO_FILE}" ]; then
-    if [ ! -f "${SO_FILE}.bak" ]; then
-      echo "Backup ${SO_FILE}"
-      cp -vfp "${SO_FILE}" "${SO_FILE}.bak"
-    fi
-    echo "Patching libsynosdk.so.7"
-    PatchELFSharp "${SO_FILE}" "SYNOFSIsRemoteFS" "B8 00 00 00 00 C3"
-  else
-    echo "libsynosdk.so.7 not found"
-  fi
   SO_FILE="/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
   if [ -f "${SO_FILE}" ]; then
     if [ ! -f "${SO_FILE}.bak" ]; then
@@ -41,11 +30,6 @@ if [ "${1}" = "late" ]; then
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon photosfacepatch - ${1}"
 
-  SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
-  if [ -f "${SO_FILE}.bak" ]; then
-    echo "Restore ${SO_FILE}"
-    mv -f "${SO_FILE}.bak" "${SO_FILE}"
-  fi
   SO_FILE="/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
   if [ -f "${SO_FILE}.bak" ]; then
     echo "Restore ${SO_FILE}"
