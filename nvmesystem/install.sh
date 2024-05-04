cd7ef613a6c24c5e1d999c9935fe2b142c6afced
Ing <wjz304@qq.com>
2024-05-04 17:18:09 +0800

    fix error

--- nvmesystem/install.sh
+++ nvmesystem/install.sh
@@ -8,11 +8,10 @@
 # From：jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
 #
 
-MODELS="SA6400"
-MODEL="$(cat /proc/sys/kernel/syno_hw_version)"
-
-if ! echo "${MODELS}" | grep -qw "${MODEL}"; then
-  echo "${MODEL} is not supported nvmesystem addon!"
+PLATFORMS="epyc7002"
+PLATFORM="$(/bin/get_key_value /etc.defaults/synoinfo.conf unique | cut -d"_" -f2)"
+if ! echo "${PLATFORMS}" | grep -qw "${PLATFORM}"; then
+  echo "${PLATFORM} is not supported nvmesystem addon!"
   exit 0
 fi
 
