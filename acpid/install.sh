3354638fef7668b1cc6a39f2ab2b0f4ee1e77837
Ing <wjz304@qq.com>
2024-06-30 06:24:27 +0800

    fix acpid

--- acpid/install.sh
+++ acpid/install.sh
@@ -11,9 +11,9 @@ if [ "${1}" = "late" ]; then
   mkdir -p "/tmpRoot/usr/rr/addons/"
   cp -vf "${0}" "/tmpRoot/usr/rr/addons/"
 
-  tar zxf /addons/acpid-7.1.tgz -C /tmpRoot/usr/
-  [ -d /tmpRoot/etc/acpi ] && rm -rf /tmpRoot/etc/acpi
-  mv -f /tmpRoot/usr/etc/acpi /tmpRoot/etc/
+  tar -zxf /addons/acpid-7.1.tgz -C /tmpRoot/usr/ ./bin ./sbin
+  tar -zxf /addons/acpid-7.1.tgz -C /tmpRoot/ ./etc ./usr
+  sed -i 's|=/|=-/|g' /tmpRoot/usr/lib/systemd/system/acpid.service
   if [ -f /usr/lib/modules/button.ko ]; then
     cp -vf /usr/lib/modules/button.ko /tmpRoot/usr/lib/modules/button.ko
   else
