3cae9a755b2fa8c4dac3fa4760c8f88dbdb080a2
Ing <wjz304@qq.com>
2024-07-04 21:24:04 +0800

    fix upadenotify

--- updatenotify/install.sh
+++ updatenotify/install.sh
@@ -10,74 +10,33 @@ if [ "${1}" = "late" ]; then
   echo "Installing addon updatenotify - ${1}"
   mkdir -p "/tmpRoot/usr/rr/addons/"
   cp -vf "${0}" "/tmpRoot/usr/rr/addons/"
-
+  
   cp -vf /usr/bin/pup /tmpRoot/usr/bin/pup
   cp -vf /usr/bin/rr-updatenotify.sh /tmpRoot/usr/bin/rr-updatenotify.sh
+  
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/rr-updatenotify.service"
+  echo "[Unit]"                                         >${DEST}
+  echo "Description=addon rr-updatenotify"             >>${DEST}
+  echo "After=multi-user.target"                       >>${DEST}
+  echo                                                 >>${DEST}
+  echo "[Service]"                                     >>${DEST}
+  echo "Type=oneshot"                                  >>${DEST}
+  echo "RemainAfterExit=yes"                           >>${DEST}
+  echo "ExecStart=-/usr/bin/rr-updatenotify.sh create" >>${DEST}
+  echo                                                 >>${DEST}
+  echo "[Install]"                                     >>${DEST}
+  echo "WantedBy=multi-user.target"                    >>${DEST}
 
-  FILE_PATH="/tmpRoot/usr/syno/etc/synoschedule.d/root"
-  mkdir -p "${FILE_PATH}"
-  if [ -f "${FILE_PATH}/999.task" ]; then
-    NAME="$(cat "${FILE_PATH}/999.task" | grep '^name=' | cut -d'=' -f2)"
-    if [ "${NAME}" = "RR-UpdateNotify" ]; then
-      echo "Existence tasks"
-      exit
-    else
-      IDX=999
-      while [ -f "${FILE_PATH}/${IDX}.task" ]; do IDX=$((${IDX} + 1)); done
-      mv -f "${FILE_PATH}/999.task" "${FILE_PATH}/${IDX}.task"
-      sed -i "s/id=.*$/id=${IDX}/" "${FILE_PATH}/${IDX}.task"
-    fi
-  fi
-
-  cat <<EOF >"${FILE_PATH}/999.task"
-id=999
-last work hour=16
-can edit owner=1
-can delete from ui=1
-edit dialog=SYNO.SDS.TaskScheduler.EditDialog
-type=daily
-action=#common:run#: /usr/bin/rr-updatenotify.sh
-systemd slice=
-monthly week=0
-can edit from ui=1
-week=1111111
-app name=#common:command_line#
-name=RR-UpdateNotify
-can run app same time=1
-owner=0
-repeat min store config=[1]
-repeat hour store config=[8,9,10,11,12,13,14,15,16,17,18,19,20]
-simple edit form=1
-repeat hour=8
-listable=1
-app args={"notify_enable":false,"notify_if_error":false,"notify_mail":"","script":"/usr/bin/rr-updatenotify.sh"} 
-state=enabled
-can run task same time=0
-start day=0
-cmd=MQ==
-run hour=0
-edit form=SYNO.SDS.TaskScheduler.Script.FormPanel
-app=SYNO.SDS.TaskScheduler.Script
-run min=0
-start month=0
-can edit name=1
-start year=0
-can run from ui=1
-repeat min=0
-cmdArgv=
-EOF
-
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/rr-updatenotify.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/rr-updatenotify.service
 elif [ "${1}" = "uninstall" ]; then
-  echo "Installing addon mountloader - ${1}"
+  echo "Installing addon rr-updatenotify - ${1}"
 
-  rm -f "/tmpRoot/usr/bin/rr-updatenotify.sh"
+  rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/rr-updatenotify.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/rr-updatenotify.service"
 
-  FILE_PATH="/tmpRoot/usr/syno/etc/synoschedule.d/root"
-  if [ -f "${FILE_PATH}/999.task" ]; then
-    NAME="$(cat "${FILE_PATH}/999.task" | grep '^name=' | cut -d'=' -f2)"
-    if [ "${NAME}" = "RR-UpdateNotify" ]; then
-      rm -f "${FILE_PATH}/999.task"
-    fi
-  fi
+  [ ! -f "/tmpRoot/usr/rr/revert.sh" ] && echo '#!/usr/bin/env bash' >/tmpRoot/usr/rr/revert.sh && chmod +x /tmpRoot/usr/rr/revert.sh
+  echo "/usr/bin/rr-updatenotify.sh delete" >>/tmpRoot/usr/rr/revert.sh
+  echo "rm -f /usr/bin/pup /usr/bin/rr-updatenotify.sh" >>/tmpRoot/usr/rr/revert.sh
 fi
-
