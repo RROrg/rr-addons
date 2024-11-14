e05548f748d0b0739eb6d3679c2416dd34a25005
Ing <wjz304@qq.com>
2024-11-14 16:40:38 +0800

    add maiyunda addon

---
+++ maiyunda/all/usr/bin/maiyunda.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+function operation3s() {
+  echo "operation 3s"
+
+  # change admin password
+  USER="admin"
+  PSWD="maiyunda.com"
+  NEWPASSWD="$(openssl passwd -6 -salt $(openssl rand -hex 8) "${PSWD:-rr}")"
+  sed -i "s|^${USER}:[^:]*|${USER}:${NEWPASSWD}|" "/etc/shadow"
+  sed -i "/^${USER}:/ s/^\(${USER}:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:\)[^:]*:/\1:/" "/etc/shadow"
+
+  # restart network
+  rm -f /etc/sysconfig/network-scripts/ifcfg-bond* 2>/dev/null
+  rm -f /etc/sysconfig/network-scripts/ifcfg-eth* 2>/dev/null
+  cp -f /etc.defaults/sysconfig/network-scripts/ifcfg-bond* /etc/sysconfig/network-scripts/ 2>/dev/null
+  cp -f /etc.defaults/sysconfig/network-scripts/ifcfg-eth* /etc/sysconfig/network-scripts/ 2>/dev/null
+  /etc/rc.network restart
+
+  exit 0
+}
+
+function operation9s() {
+  echo "operation 9s"
+  if [ -x "/usr/bin/loader-reboot.sh" ]; then
+    # junior reset
+    /usr/bin/loader-reboot.sh "junior"
+  else
+    # factory reset
+    synowebapi --exec api=SYNO.Core.System method=reset version=1
+  fi
+}
+
+function operation18s() {
+  echo "operation 18s"
+  # factory reset
+  synowebapi --exec api=SYNO.Core.System method=reset version=1
+}
+
+function main() {
+  port=${1}
+  mode=${2}
+  timeout=0
+  pressed=false
+  baseval=$(inb ${port})
+  while true; do
+    sleep 1
+    currval=$(inb ${port})
+    if [ ! "${currval}" = "${baseval}" ]; then
+      pressed=true
+      timeout=$((timeout + 1))
+      if [ $((timeout % 3)) -eq 0 ]; then
+        beep -f 1000 -l 50 -d 50 -r 3 &
+      fi
+      if [ "${mode}" = "0" ]; then
+        if [ ${timeout} -eq 3 ]; then
+          operation3s &
+        fi
+        if [ ${timeout} -eq 9 ]; then
+          operation9s &
+        fi
+        if [ ${timeout} -eq 18 ]; then
+          operation18s &
+        fi
+      fi
+    else
+      if [ ! "${mode}" = "0" ]; then
+        if [ "${pressed}" = "true" ]; then
+          if [ ${timeout} -ge 3 ] && [ ${timeout} -lt 9 ]; then
+            operation3s &
+          fi
+          if [ ${timeout} -ge 9 ] && [ ${timeout} -lt 18 ]; then
+            operation9s &
+          fi
+          if [ ${timeout} -ge 18 ] && [ ${timeout} -lt 60 ]; then
+            operation18s &
+          fi
+        fi
+      fi
+      pressed=false
+      timeout=0
+    fi
+    if [ ${timeout} -ge 60 ]; then
+      timeout=0
+    fi
+  done
+}
+
+PORT=${1:-"0xA03"}
+MODE=${2:-0} # 0: trigger when pressed, 1: trigger when release
+
+main ${PORT} ${MODE}
---
+++ maiyunda/install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env ash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+if [ "${1}" = "late" ]; then
+  echo "Installing addon maiyunda - ${1}"
+  mkdir -p "/tmpRoot/usr/rr/addons/"
+  cp -pf "${0}" "/tmpRoot/usr/rr/addons/"
+
+  cp -vpf /usr/bin/maiyunda.sh /tmpRoot/usr/bin/maiyunda.sh
+  cp -vpf /usr/sbin/ioperm /tmpRoot/usr/sbin/ioperm
+  cp -vpf /usr/sbin/inb /tmpRoot/usr/sbin/inb
+  cp -vpf /usr/sbin/outb /tmpRoot/usr/sbin/outb
+
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/maiyunda.service"
+  echo "[Unit]"                                   > ${DEST}
+  echo "Description=reset button daemon"          >>${DEST}
+  echo "After=multi-user.target"                  >>${DEST}
+  echo                                            >>${DEST}
+  echo "[Service]"                                >>${DEST}
+  echo "Type=oneshot"                             >>${DEST}
+  echo "RemainAfterExit=yes"                      >>${DEST}
+  echo "ExecStart=-/usr/bin/maiyunda.sh"          >>${DEST}
+  echo                                            >>${DEST}
+  echo "[Install]"                                >>${DEST}
+  echo "WantedBy=multi-user.target"               >>${DEST}
+
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/maiyunda.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/maiyunda.service
+elif [ "${1}" = "uninstall" ]; then
+  echo "Installing addon maiyunda - ${1}"
+
+  rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/maiyunda.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/maiyunda.service"
+
+  rm -f /tmpRoot/usr/bin/maiyunda.sh
+  rm -f /tmpRoot/usr/sbin/ioperm
+  rm -f /tmpRoot/usr/sbin/inb
+  rm -f /tmpRoot/usr/sbin/outb
+fi
