7ba8620719e835bfc243abcc0fa8151c7168fb3e
Ing <wjz304@qq.com>
2025-02-08 15:30:48 +0800

    移除 日志 “Can't get sata chip name from pattern”

--- trivial/install.sh
+++ trivial/install.sh
@@ -11,7 +11,7 @@ if [ "${1}" = "late" ]; then
   mkdir -p "/tmpRoot/usr/rr/addons/"
   cp -pf "${0}" "/tmpRoot/usr/rr/addons/"
 
-  # logger
+  # libsynosata.so.1
   SO_FILE="/tmpRoot/usr/lib/libsynosata.so.1"
   [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
   cp -pf "${SO_FILE}" "${SO_FILE}.tmp"
@@ -19,10 +19,22 @@ if [ "${1}" = "late" ]; then
     sed "s/8d15ba160000bf03000000e8c0c8ffff/8d15ba160000bf030000009090909090/" |
     xxd -r -p >"${SO_FILE}" 2>/dev/null
   rm -f "${SO_FILE}.tmp"
-  
+
+  # libhwcontrol.so.1
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
+  cp -pf "${SO_FILE}" "${SO_FILE}.tmp"
+  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
+    sed "s/8d1512840900bf03000000e8d0fcfdff/8D1512840900BF030000009090909090/; s/8d159a810900bf03000000e858fafdff/8D159A810900BF030000009090909090/" |
+    xxd -r -p >"${SO_FILE}" 2>/dev/null
+  rm -f "${SO_FILE}.tmp"
+
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon trivial - ${1}"
 
   SO_FILE="/tmpRoot/usr/lib/libsynosata.so.1"
+  [ -f "${SO_FILE}.bak" ] && mv -f "${SO_FILE}.bak" "${SO_FILE}"
+
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
   [ -f "${SO_FILE}.bak" ] && mv -f "${SO_FILE}.bak" "${SO_FILE}"
 fi
