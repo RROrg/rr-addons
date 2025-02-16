4dcb2046f97045e7bf3c195c52c188e33e13eac5
Ing <wjz304@qq.com>
2025-02-17 02:21:03 +0800

    在 eudev 中添加对 eject 命令的复制检查

--- eudev/install.sh
+++ eudev/install.sh
@@ -47,6 +47,8 @@ elif [ "${1}" = "late" ]; then
   [ ! -L "/tmpRoot/usr/sbin/modinfo" ] && ln -vsf /usr/bin/kmod /tmpRoot/usr/sbin/modinfo
   [ ! -L "/tmpRoot/usr/sbin/depmod" ] && ln -vsf /usr/bin/kmod /tmpRoot/usr/sbin/depmod
 
+  [ ! -f "/tmpRoot/usr/bin/eject" ] && cp -vpf /usr/bin/eject /tmpRoot/usr/bin/eject
+
   echo "copy modules"
   export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
   isChange=false
