81b1590394ebd150df66d68ffc2999ef5c457317
Ing <wjz304@qq.com>
2025-05-16 16:40:35 +0800

    更新 eudev 增加传感器模块支持

--- eudev/install.sh
+++ eudev/install.sh
@@ -38,7 +38,7 @@ elif [ "${1}" = "modules" ]; then
   /usr/sbin/modprobe pcspeaker
   /usr/sbin/modprobe pcspkr
   # modprobe modules for the sensors
-  for I in coretemp k10temp hwmon-vid it87 nct6683 nct6775 adt7470 adt7475; do
+  for I in coretemp k10temp hwmon-vid it87 nct6683 nct6775 adt7470 adt7475 adm1021 adm1031 adm9240 lm75 lm78 lm90; do
     /usr/sbin/modprobe "${I}"
   done
 
