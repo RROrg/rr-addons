9bb73931e0b91dcb9759d443357d3110bc7d2ce3
Ing <wjz304@qq.com>
2025-06-11 11:41:02 +0800

    优化 smartctl.sh 脚本，替换参数中的 ata 为 auto，并添加设备类型处理逻辑

--- smartctl/all/usr/bin/smartctl.sh
+++ smartctl/all/usr/bin/smartctl.sh
@@ -6,6 +6,39 @@
 # See /LICENSE for more information.
 #
 
+#  -d TYPE, --device=TYPE
+#         Specify device type to one of: ata, scsi, nvme[,NSID], sat[,auto][,N][+TYPE], usbcypress[,X], usbjmicron[,p][,x][,N], usbsunplus, marvell, areca,N/E, 3ware,N, hpt,L/M/N, megaraid,N, aacraid,H,L,ID, cciss,N, auto, test
+
+#  smartctl -d ata -A /dev/sdh
+
 args=()
-for arg in "$@"; do [[ "$arg" = "ata" ]] && args+=("auto") || args+=("$arg"); done # 替换参数中的 ata 为 auto
+
+TYPE=ata
+# for argv in "$@"; do
+#   if [ -e "${argv}" ]; then
+#     info=$(/usr/bin/smartctl.bak -i "${argv}" 2>/dev/null)
+#     if echo "${info}" | grep -q "NVMe"; then
+#       TYPE="nvme"
+#     elif echo "${info}" | grep -q "SATA Version\|ATA Version"; then
+#       TYPE="ata"
+#     elif echo "${info}" | grep -q "Transport protocol: SAS"; then
+#       TYPE="scsi"
+#     elif echo "${info}" | grep -q "Unknown USB bridge"; then
+#       TYPE="usbcypress"
+#     else
+#       TYPE="auto"
+#     fi
+#   fi
+# done
+
+argp=""
+for argv in "$@"; do
+  if [ "${argp}" = "-d" ] && [ "${argv}" = "ata" ]; then
+    args+=("${TYPE}")
+  else
+    args+=("${argv}")
+  fi
+  argp="${argv}"
+done
+
 /usr/bin/smartctl.bak "${args[@]}"
